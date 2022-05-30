544896ea37daad06c28f449f3c25a8da43e44a14
Robson Vieira <robson.ppmg@gmail.com>
2022-05-30 08:38:08 -0300

    adicionando manual do projeto

--- app/app.py
+++ app/app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from .routes.usuario import usuario
-from .routes.produto import produto
+from .routes.cliente import cliente
 from .extentions import database
 from .commands.userCommands import userCommands
 
@@ -10,7 +10,7 @@ def create_app(config_object="app.settings"):
     app.config.from_object(config_object)
 
     app.register_blueprint(usuario)
-    app.register_blueprint(produto)
+    app.register_blueprint(cliente)
 
     app.register_blueprint(userCommands)
     database.init_app(app)
